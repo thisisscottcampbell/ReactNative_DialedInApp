723035eb6451c8d880937f01f7859d1582edd2d2
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-28 16:06:47 -0400

    timer

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const minsToMills = (num) => num * 1000 * 60;
 const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
 export const CountDown = ({ min = 20, isPaused }) => {
+	const interval = useRef(null);
+
 	const [time, setTime] = useState(minsToMills(min));
 
 	const minutes = Math.floor(time / 1000 / 60) % 60;
 	const seconds = Math.floor(time / 1000) % 60;
+
+	const timer = () => {
+		setTime((time) => {
+			if (time === 0) return time;
+			const timeLeft = time - 1000;
+
+			return timeLeft;
+		});
+	};
+
+	useEffect(() => {
+		interval.current = setInterval(timer, 1000);
+
+		return () => clearInterval(interval.current);
+	}, []);
 
 	return (
 		<Text style={styles.text}>
