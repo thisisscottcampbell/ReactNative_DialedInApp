c27d9bc981583f5d8ccc2b3a27f11244ef397510
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-28 16:15:16 -0400

    timer funntionality

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { CountDown } from '../../components/CountDown';
+import { RoundedButton } from '../../components/RoundedButton';
 
 export const Timer = ({ subject }) => {
+	const [isStarted, setStarted] = useState(false);
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.countDown}>
-				<CountDown />
+				<CountDown isPaused={!isStarted} />
 			</View>
 			<View style={{ paddingTop: 40 }}>
 				<Text style={styles.title}>Focusing On:</Text>
 				<Text style={styles.task}>{subject}</Text>
+			</View>
+			<View style={styles.buttonWrapper}>
+				<RoundedButton title=">" size={50} onPress={() => setStarted(true)} />
+				<RoundedButton title="||" size={50} onPress={() => setStarted(false)} />
 			</View>
 		</View>
 	);
@@ -33,5 +40,11 @@ const styles = StyleSheet.create({
 		flex: 0.5,
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	buttonWrapper: {
+		flex: 0.3,
+		padding: 15,
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 });
