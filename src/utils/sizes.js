461a809b89da0e51dabce1d437b1dc0bfbdba8d2
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-27 20:00:26 -0400

    utils

---
+++ src/utils/sizes.js
@@ -0,0 +1,17 @@
+export const fontSize = {
+	sm: 8,
+	md: 16,
+	lg: 24,
+	xl: 32,
+	xxl: 40,
+	xxxxl: 80,
+};
+
+export const paddingSize = {
+	sm: 8,
+	md: 16,
+	lg: 24,
+	xl: 32,
+	xxl: 40,
+	xxxxl: 80,
+};
